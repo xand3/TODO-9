68931cf382b93c2cb416d2ae1bfeab0737020f8a
Alexandre Bastos <xande.bastos321@gmail.com>
2022-09-02 15:24:46 -0300

    Feito o header do layout

---
+++ todo-react/src/components/Header.jsx
@@ -0,0 +1,20 @@
+export function Header() {
+    return (
+		<div className="header-content">
+			<div className="header">
+            <p>uma seleção de produtos</p>
+            <h1>especial para você</h1>
+            <p>
+                Todos os produtos desta lista foram selecionados a partir da sua
+                navegação. Aproveite!
+            </p>
+			<div className="btns">
+				<button>Conheça a Linx</button>
+				<button>Ajude o algoritmo</button>
+				<button>Seus produtos</button>
+				<button>Compartilhe</button>
+			</div>
+        </div>
+		</div>
+    );
+}
