fce696731a5c080ef83b417d4aaa12f6ef9ab50e
Alexandre Bastos <xande.bastos321@gmail.com>
2022-09-02 22:37:39 -0300

    Finalizada a primeira listagem de produtos

--- todo-react/src/components/Products.jsx
+++ todo-react/src/components/Products.jsx
@@ -1,29 +1,42 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
-
-const url = 'https://frontend-intern-challenge-api.iurykrieger.vercel.app/products?page=1'
 
 export function Products() {
     const [products, setProdutos] = useState([]);
 
-	useEffect(() => {
-		async function fetchData() {
-			const res =  await(fetch(url))
+    const url =
+        "https://frontend-intern-challenge-api.iurykrieger.vercel.app/products?page=1";
 
-			const data = await res.json()
+    useEffect(() => {
+        fetch(url)
+            .then((res) => res.json())
+            .then((data) => setProdutos(data.products));
+    }, []);
 
-			setProdutos(data.products)
-		}
-		fetchData()
-	}, [''])
-	console.log(products)
-	
     return (
         <div className="products-content">
             <fieldset>
                 <legend>Sua seleção especial</legend>
             </fieldset>
+            <div className="products">
+                {products.map((product) => {
+                    return (
+                        <div className="products-area">
+                            <div key={product.id} className="card-product">
+                                <img src={product.image} alt="" />
+                                <p className="product-name">{product.name}</p>
+                                <p>{product.description}</p>
+                                <p>De: R${product.oldPrice}</p>
+                                <p className="atual-price">Por: R${product.price}</p>
+                                <p>ou 2x de R${product.price / 2}</p>
+                                <button>Comprar</button>
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
+			<div className="btn">
+				<button>Ainda mais produtos aqui!</button>
+			</div>
         </div>
     );
 }
-
